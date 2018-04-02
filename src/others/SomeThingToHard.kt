475635b30152c5186a5bc1e0300968e6363e1f61
Vintage appMaker <adsloader99@gmail.com>
2018-04-02 22:17:00 +0900

    VintageappMaker 관리

---
+++ src/others/SomeThingToHard.kt
@@ -0,0 +1,26 @@
+package others
+
+fun main(args: Array<String>) {
+
+    // 1.
+    // 변수(함수를 포함한 어떤 형이던) 대입을 할 경우,
+    // 제어문을 사용할 수 있다.
+    var s1 = 3
+    var a  = when(s1){
+        3    -> { s1 * 3}
+        else -> { s1    }
+    }
+    println (a)
+
+    // 2.
+    // 엘비스오퍼레이터 ?:
+    // 저 문자가 우로 90도 회전시
+    // 엘비스프레슬리 모습이라고 한다.
+    var s2 : String? = "AAAA"
+    println( s2?.length ?: -1)
+
+    // 3.
+    // null safe를 위해 사용하는 let
+    var s3 : Int? = null
+    s3?.let{ println (it * 2) }
+}
